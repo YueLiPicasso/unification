a2051c7f594169d1d658c7cadc26b283998e1b9e
YueLiPicasso <YueLiPicasso@users.noreply.github.com>
2015-05-27 16:28:01 +0100

    Third commit

--- UnifyTerms.hs
+++ UnifyTerms.hs
@@ -1,6 +1,6 @@
 module UnifyTerms
    (
-
+     unifyTerms
    ) where
 
 import Substitution
@@ -24,11 +24,13 @@ unifyTerms (Constant _ ) (Function _ _ _ ) = Nothing
 
 unifyTerms (Function _ _ _ ) (Constant _ ) = Nothing
 
-unifyTerms f@(Function _ _ tms ) v@(Variable _ ) =
-     | v `occursAt` tms = Nothing
+unifyTerms f@(Function _ _ tms ) v@(Variable _ )
+     | v `occursAt` f   = Nothing
      | otherwise        = Just [(f,v)]
 --occurs check needed
-unifyTerms v@(Variable _ ) f@(Function _ _ _ )  = Just [(f,v)]
+unifyTerms v@(Variable _ ) f@(Function _ _ tms )
+    | v `occursAt` f   = Nothing
+    | otherwise        = Just [(f,v)]
 
 unifyTerms (Function name1 arity1 tms1) (Function name2 arity2 tms2)
    | name1 /= name2              = Nothing
@@ -37,13 +39,19 @@ unifyTerms (Function name1 arity1 tms1) (Function name2 arity2 tms2)
    | (null tms1) && ((not . null) tms2)  = Nothing
    | (null tms2) && ((not . null) tms1)  = Nothing
    | (length tms1) /= (length tms2)      = Nothing
-   | otherwise                   = concat (zipWith unifyTerms tms1 tms2)
+   | otherwise                   = unifyTermList tms1 tms2
 
 ------------------------------------------------------------
 occursAt :: Term -> Term -> Bool
-v@(Variable str) `occursAt` (Funtion _ _ tms) = or (map (occursAt v) tms)
+v@(Variable str) `occursAt` (Function _ _ tms) = or (map (occursAt v) tms)
 (Variable str1 ) `occursAt` (Variable str2 )
        | str1 == str2      = True
        | otherwise         = False
 (Variable _ ) `occursAt` (Constant _ ) = False
 _ `occursAt` _ = undefined
+---------------------------------------------------------------
+unifyTermList :: [Term] -> [Term] -> Maybe Substitution
+--the context in which unifyTermList is called requires the two
+-- input list non empty and of the same length
+unifyTermList (t1:[]) (t2:[]) = unifyTerms t1 t2
+unifyTermList _ _ = undefined
